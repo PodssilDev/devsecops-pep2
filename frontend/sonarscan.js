a503149123c0a38f6e751c5f611847e46979afdc
PodssilDev <john.serrano@usach.cl>
2024-11-27 01:10:50 -0300

    sonar

--- frontend/sonarscan.js
+++ frontend/sonarscan.js
@@ -3,11 +3,11 @@ const scanner = require("sonarqube-scanner");
 scanner(
   {
     serverUrl: "http://localhost:9000",
-    token: "sqp_65bd5cf13258a2e0a554d61f6ca6c5f29d8b5aec",
+    token: "sqp_a060ef800c0ba67b9f51b6b4e4216bca80572177",
     options: {
-      "sonar.projectName": "sonarqube-react-project",
+      "sonar.projectName": "frontend",
       "sonar.projectDescription": "Here I can add a description of my project",
-      "sonar.projectKey": "CallBase-Frontend",
+      "sonar.projectKey": "frontend",
       "sonar.projectVersion": "0.0.1",
       "sonar.exclusions": "sonarscan.js, .gitignore, Dockerfile, src/components/Server.jsx, server.js",
       "sonar.sourceEncoding": "UTF-8",
